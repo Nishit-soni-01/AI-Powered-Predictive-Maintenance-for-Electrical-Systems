3d193ddb6bdd6746cffc9b5c5d1d4cbf3b5081b5
Nishit Soni <ns2004.soni@gmail.com>
2026-06-23 22:35:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# ── Page config ──────────────────────────────────────────────────────────────
+
 st.set_page_config(
     page_title="Predictive Maintenance AI",
     page_icon="⚡",
